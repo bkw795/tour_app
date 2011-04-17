7110fa1dfac5816c242dc10d70c6579ad50bd9f1
Brian Widerman <bkw795@gmail.com>
2011-04-17 19:58:33 +0000

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+gem 'mysql', '2.8.1'
+
+group :development do
+  gem 'rspec-rails', '2.0.1'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TourApp::Application.config.secret_token = '353930ae4fa6e4892bd8ebf239558bbae298da6f3dae40902dd27aec3f95e97944a2d851d9adc762da98c122297f409642d16343344d60deaa3fe42848b39fee'
